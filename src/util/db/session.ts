0c345fb7daca53489dffc4094b8b61b9cb2ecc16
shaishabcoding <shaishabchandrashil@gmail.com>
2025-07-06 20:04:45 +0600

    Refactor: Implement database session handling with transaction support check
    This commit refactors the `useSession` utility function to:
    
    -   Check if the MongoDB database supports sessions by attempting a test query in a transaction once.
    -   Run the provided function `fn` with a session and transaction if supported.
    -   Run the function `fn` without a session if sessions are not supported.
    -   Log whether the database supports sessions for debugging purposes.

--- src/util/db/session.ts
+++ src/util/db/session.ts
@@ -1,9 +1,43 @@
 /* eslint-disable no-unused-vars */
 import { ClientSession, startSession } from 'mongoose';
+import User from '../../app/modules/user/User.model';
+import { logger } from '../logger/logger';
+import colors from 'colors';
 
+let supportSession: boolean | null = null;
+
+/**
+ * Runs a function inside a MongoDB session and transaction if supported.
+ *
+ * Checks once if the database supports sessions by running a test query in a transaction.
+ * If supported, runs `fn` with a session and transaction.
+ * If not supported, runs `fn` without a session.
+ */
 export const useSession = async <T>(
-  fn: (session: ClientSession) => Promise<T>,
+  fn: (session: ClientSession | null) => Promise<T>,
 ): Promise<T> => {
+  if (supportSession === null) {
+    const session = await startSession();
+    session.startTransaction();
+    try {
+      await User.exists({}).session(session);
+      await session.commitTransaction();
+      supportSession = true;
+    } catch (error) {
+      await session.abortTransaction();
+      supportSession = false;
+    } finally {
+      session.endSession();
+      logger.info(
+        colors.yellow(
+          `Database ${supportSession ? 'supports' : 'does not support'} sessions`,
+        ),
+      );
+    }
+  }
+
+  if (supportSession === false) return fn(null);
+
   const session = await startSession();
   session.startTransaction();
   try {
